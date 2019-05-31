a1240fba236742325c9a3208435a2eedaf2edcad
Guy <lionel.adossou@gmail.com>
2019-05-31 02:57:15 +0200

    update comments

--- app.py
+++ app.py
@@ -7,14 +7,16 @@ from aspect import check_if_user_exists, log
 from services.manager import ResourceManager
 from services.users import User
 
-#jointPoint
+#Aspect Weaver
 aspectlib.weave(ResourceManager, check_if_user_exists)
 aspectlib.weave(ResourceManager, log)
 
 user1 = User("toto", "xxx")
 user1_ressource = ResourceManager(user1)
+#jointPoint
 user1_ressource.add("data")
 
 user2 = User("not_exist", "xxx")
 user2_ressource = ResourceManager(user2)
+#jointPoint
 user2_ressource.add("data")
